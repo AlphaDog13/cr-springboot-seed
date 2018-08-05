77b21af4058ef3f10252d4fd830793218d336af6
AlphaDog <AlphaDog@AlphaDogs-MacBook-Pro.local>
2018-08-05 11:10:42 +0800

    v0.0.1

---
+++ src/main/java/com/cyberunner/seed/CrSpringbootSeedApplication.java
@@ -0,0 +1,12 @@
+package com.cyberunner.seed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrSpringbootSeedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrSpringbootSeedApplication.class, args);
+	}
+}
